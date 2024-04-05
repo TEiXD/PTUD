29edf37af54b073e819acb17f17cfc5474204504
Hứa Quang Cường <122151096+TEiXD@users.noreply.github.com>
2024-04-05 22:00:16 +0700

    update :D

--- src/main/java/DAO/NhanVienDAO.java
+++ src/main/java/DAO/NhanVienDAO.java
@@ -5,7 +5,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import connectDB.ConnectDB;
 import entity.NhaGa;
@@ -13,36 +12,36 @@ import entity.NhanVien;
 
 public class NhanVienDAO {
 	//Add DS
-	public ArrayList<NhanVien> layThongTin(){
-		ArrayList<NhanVien> dsNhanVien = new ArrayList<NhanVien>();
-		try {
-			ConnectDB.getInstance().connect();
-			Connection conn = ConnectDB.getConnection();
-			String SQL = "SELECT nv.maNV, nv.hoTen, nv.CCCD, nv.gioiTinh, nv.SDT, nv.email, nv.ngaySinh, nv.trinhDo, ng.NhaGa " +
-	                "FROM NhanVien nv  " +
-	                "INNER JOIN NhaGa ng ON nv.maNhaGa = ng.maNhaGa " ;
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(SQL);
-			while(rs.next()) {
-				String maNV = rs.getString(1);
-				String hoten = rs.getString(2);
-				String CCCD = rs.getString(3);
-				String gioiTinh = rs.getString(4);
-				String SDT = rs.getString(5);
-				String email = rs.getString(6);
-				Date ngaySinh = rs.getDate(7);
-				String trinhDo = rs.getString(8);
-				String maNhaGa = rs.getString(9);
-                NhaGa ng = new NhaGa(maNhaGa);
-                                
-				NhanVien nv = new NhanVien(maNV, hoten, CCCD, gioiTinh, SDT, email, ngaySinh, trinhDo, ng);
-				dsNhanVien.add(nv);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return dsNhanVien;
+	public ArrayList<NhanVien> layThongTin() {
+	    ArrayList<NhanVien> dsNhanVien = new ArrayList<>();
+	    try {
+	        Connection conn = ConnectDB.getConnection();
+	        String SQL = "SELECT nv.maNV, nv.hoTen, nv.CCCD, nv.gioiTinh, nv.SDT, nv.email, nv.ngaySinh, nv.trinhDo, ng.NhaGa " +
+	                	 "FROM NhanVien nv " +"INNER JOIN NhaGa ng ON nv.maNhaGa = ng.maNhaGa";
+
+	        PreparedStatement ps = conn.prepareStatement(SQL);
+	        ResultSet rs = ps.executeQuery();
+	        while (rs.next()) {
+	            String maNV = rs.getString("maNV");
+	            String hoten = rs.getString("hoTen");
+	            String CCCD = rs.getString("CCCD");
+	            String gioiTinh = rs.getString("gioiTinh");
+	            String SDT = rs.getString("SDT");
+	            String email = rs.getString("email");
+	            Date ngaySinh = rs.getDate("ngaySinh");
+	            String trinhDo = rs.getString("trinhDo");
+	            String maNhaGa = rs.getString("NhaGa");
+
+	            NhaGa ng = new NhaGa(maNhaGa);
+	            NhanVien nv = new NhanVien(maNV, hoten, CCCD, gioiTinh, SDT, email, ngaySinh, trinhDo, ng);
+	            dsNhanVien.add(nv);
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return dsNhanVien;
 	}
+	
 	//Them NV
 	public boolean addNV(NhanVien nhanVien) {
 		ConnectDB.getInstance();
@@ -66,42 +65,42 @@ public class NhanVienDAO {
 		}
 		return n>0;
 	}
+	
 	//xoa NV
 	public boolean xoaNV(String maNV) {
-		ConnectDB.getInstance();
-		Connection conn = ConnectDB.getConnection();
-		PreparedStatement st = null;
-		int n = 0;
-		try {
-			String SQL = "Delete from dbo.NhanVien where maNV = ?";
-			st = conn.prepareStatement(SQL);
-			st.setString(1, maNV);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return n>0;
+	    try (Connection conn = ConnectDB.getConnection();
+	         PreparedStatement st = conn.prepareStatement("DELETE FROM dbo.NhanVien WHERE maNV = ?")) {
+	        st.setString(1, maNV);
+	        int rowsAffected = st.executeUpdate();
+	        return rowsAffected > 0;
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
 	}
+
+	
 	//Sua NV
-	public boolean SuaNV(NhanVien nhanVien){
-		ConnectDB.getInstance();
-		Connection conn = ConnectDB.getConnection();
-		PreparedStatement st = null;
-		int n = 0;
-		try {
-			String SQL = "Update dbo.NhanVien set SET maNV = ?, HoTen = ?, CCCD = ?, GioiTinh = ?, SDT = ?, Email = ?, NgaySinh = ?, TrinhDo = ?, MaNhaGa = ? WHERE maNV = ?";
-			st = conn.prepareStatement(SQL);
-			st.setString(1, nhanVien.getMaNV());
-			st.setString(2, nhanVien.getHoTen());
-			st.setString(3, nhanVien.getCCCD());
-			st.setString(4, nhanVien.getGioiTinh());
-			st.setString(5, nhanVien.getSDT());
-			st.setString(6, nhanVien.getEmail());
-			st.setObject(7, nhanVien.getNgaySinh());
-			st.setString(8, nhanVien.getTrinhDo());
-			st.setString(9, nhanVien.getNhaGa().getMaNhaGa().trim());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return n>0;
+	public boolean SuaNV(NhanVien nhanVien) {
+	    try (Connection conn = ConnectDB.getConnection();
+	         PreparedStatement st = conn.prepareStatement(
+	                 "UPDATE dbo.NhanVien SET maNV = ?, HoTen = ?, CCCD = ?, GioiTinh = ?, SDT = ?, Email = ?, NgaySinh = ?, TrinhDo = ?, MaNhaGa = ? WHERE maNV = ?")) {
+	        st.setString(1, nhanVien.getMaNV());
+	        st.setString(2, nhanVien.getHoTen());
+	        st.setString(3, nhanVien.getCCCD());
+	        st.setString(4, nhanVien.getGioiTinh());
+	        st.setString(5, nhanVien.getSDT());
+	        st.setString(6, nhanVien.getEmail());
+	        st.setObject(7, nhanVien.getNgaySinh());
+	        st.setString(8, nhanVien.getTrinhDo());
+	        st.setString(9, nhanVien.getNhaGa().getMaNhaGa().trim());
+
+	        int rowsAffected = st.executeUpdate();
+	        return rowsAffected > 0;
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
 	}
+
 }
--- src/main/java/connectDB/ConnectDB.java
+++ src/main/java/connectDB/ConnectDB.java
@@ -1,34 +1,41 @@
-package connectDB;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectDB {
-	public static Connection conn = null;
-	private static ConnectDB instance = new ConnectDB();
+    private static final String DB_URL = "jdbc:sqlserver://localhost\\\\Tei-Laptop:1433;databaseName=BanVeTau;integratedSecurity=false;encrypt=false;trustServerCertificate=true;";
+    private static final String DB_USER = "tei";
+    private static final String DB_PASSWORD = "29032004";
 
-	public static ConnectDB getInstance() {
-		return instance;
-	}
+    private static Connection conn = null;
+    private static ConnectDB instance = new ConnectDB();
 
-	public Connection connect() throws SQLException {
-		String url = "jdbc:sqlserver://localhost\\\\Tei-Laptop:1433;databaseName=BanVeTau;integratedSecurity=false;encrypt=false;trustServerCertificate=true;";
-		String user = "tei";
-		String password = "29032004";
-		return conn = DriverManager.getConnection(url, user, password);
-	}
+    public static ConnectDB getInstance() {
+        return instance;
+    }
 
-	public void disconnect() {
-		if (conn != null)
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-	}
+    public Connection connect() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            // Handle the exception (log, throw, or return null)
+        }
+        return conn;
+    }
 
-	public static Connection getConnection() {
-		return conn;
-	}
+    public void disconnect() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Connection getConnection() {
+        return conn;
+    }
 }
